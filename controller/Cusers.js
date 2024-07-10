f7a70d58889d7a4d5884e213ac4641f26be090c5
AHNDAEUN <dean034@naver.com>
2024-07-10 17:25:51 +0900

    users mvc

--- controller/Cusers.js
+++ controller/Cusers.js
@@ -1,12 +1,27 @@
 /*
 추가: model 불러오기
 */
+const { users } = require('../models/Muser')
 
-exports.main = (req, res) => {
-    console.log("GET / MAIN")
-    res.render('index');
-}
 
-exports.main = (req, res) => {
-    
-}
+
+        exports.getUsers = (req, res) => {
+            console.log("GET / MAIN")
+            res.render('users');
+        }
+
+    exports.postUsers = async (req, res) => {
+    try {
+        console.log(req.body)
+        const {user_id, user_name, user_pw,birth_day,profile_img} = req.body;
+        const newUser = await users.create({
+            user_id, user_name, user_pw,birth_day,profile_img
+        });
+
+        res.json(newUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+
+    }
