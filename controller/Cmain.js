f519c9027a59484b219333e167fbb7f6b4b12c56
“errorose” <yoonyeseul2000@gmail.com>
2024-07-12 00:14:58 +0900

    feat: Cmain.js 코드 수정

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -1,6 +1,5 @@
-
-const {Recipes, Recipe_Img, Users} = require('../models/Mindex');
-
+// 모델 가져오기
+const { Recipes, Users, Recipe_Img } = require('../models/Mindex');
 
 // main page
 exports.main = (req,res) => {
@@ -8,27 +7,30 @@ exports.main = (req,res) => {
 }
 
 // 메인 페이지 리스트 (주재료에 대한 이미지, 제목, 작성자 가져오기)
-exports.recipeList = async (req, res)=>{
-    try{
+exports.recipeList = async (req, res) => {
+    try {
         console.log(req.params.main_ingredient);
-        const {main_ingredient} = req.params;
+        const { main_ingredient } = req.params;
 
         const lists = await Recipes.findAll({
-            where: {main_ingredient},
-            include: [{
-                model: Users,
-                attributes: ['user_id']
-            },
-            {
-                model: Recipe_Img,
-                attributes: ['image_num']
-            }
+            where: { main_ingredient },
+            include: [
+                {
+                    model: Users,
+                    attributes: ['user_name']
+                },
+                {
+                    model: Recipe_Img,
+                    where: { main_img: 1 },
+                    attributes: ['image_url'],
+                    required: false // 메인 이미지가 없는 레시피도 포함되도록 설정
+                }
             ],
-            attributes: ['title', 'user_name']
+            attributes: ['title']
         });
         res.json(lists);
-    }catch(error){
+    } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
-}
+};
