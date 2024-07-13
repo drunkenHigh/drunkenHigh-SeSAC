e45befa8069c846db1a1f175df91ace1b2447bc4
“errorose” <yoonyeseul2000@gmail.com>
2024-07-14 03:21:32 +0900

    feat: 쿠키 수정, 레시피 리스트............

--- controller/Cmain.js
+++ controller/Cmain.js
@@ -1,46 +1,56 @@
 // 모델 가져오기
 const { Recipes, Users, Recipe_Img } = require('../models/Mindex');
-const { setCookie } = require('./Ccookie');
 
 // main page
-exports.main = async (req,res) => {
-    setCookie(req, res);
-    // await this.getRecipeList(req, res);
-    res.render('index');
-}
+exports.main = async (req, res) => {
+    try {
+        // 쿼리 파라미터에서 주재료 가져오기
+        const { main_ingredient } = req.query
+
+        // getRecipeList 함수 호출 (주재료가 있을 경우 필터링, 없을 경우 모든 레시피 가져오기)
+        const recipes = await getRecipeList({ main_ingredient: main_ingredient || '' });
+
+        res.render('index', { recipes });  // 'index' 페이지에 recipes 데이터를 전달
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
 
 // 레시피 리스트 가져오기 (전체, 주재료)
-exports.getRecipeList = async (req, res) => {
+exports.getRecipeList = async (filters = {}) => {
     try {
-        const { main_ingredient } = req.params;
+        // 주재료 필터링 (기본값은 빈 객체, 즉 필터링 없음)
+        const { main_ingredient } = filters;
+
+        // 주재료가 있을 경우 필터링, 없을 경우 모든 레시피 가져오기
         const whereClause = main_ingredient ? { main_ingredient } : {};
 
-        if (main_ingredient) {
-            // main_ingredient가 존재할 경우 조건을 추가
-            whereClause = { main_ingredient };
-        }
-
+        // 레시피 조회
         const lists = await Recipes.findAll({
             where: whereClause,
             include: [
                 {
                     model: Users,
-                    attributes: ['user_name']
+                    attributes: ['user_name']  // 작성자
                 },
                 {
                     model: Recipe_Img,
-                    where: { main_img: 1 },
+                    where: { main_img: 1 },  // 메인 이미지
                     attributes: ['image_url'],
-                    required: false 
+                    required: false  // 이미지가 없을 시에도 가져오기
                 }
             ],
-            attributes: ['title'],
-            order: [['createdAt', 'DESC']] 
+            attributes: ['title'],  // 제목
+            order: [['createdAt', 'DESC']]  // 최신 레시피부터 정렬
         });
-        res.json(lists);
+
+        // 레시피 리스트를 반환
+        return lists;
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        throw new Error('Internal Server Error');  
     }
 };
 
@@ -49,61 +59,6 @@ exports.getRecipeList = async (req, res) => {
 
 
 
-//
-// 모든 레시피 가져오기 (이미지, 제목, 작성자)
-exports.getRecipeListAll = async (req, res) => {
-    try {
-        const lists = await Recipes.findAll({
-            include: [
-                {
-                    model: Users,
-                    attributes: ['user_name']
-                },
-                {
-                    model: Recipe_Img,
-                    where: { main_img: 1 },
-                    attributes: ['image_url'],
-                    required: false 
-                }
-            ],
-            attributes: ['title'],
-            order: [['createdAt', 'DESC']] 
-        });
-        res.json(lists);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-};
 
-// 주재료에 대한 레시피 리스트 가져오기 (이미지, 제목, 작성자)
-exports.getRecipeListMain = async (req, res) => {
-    try {
-        console.log(req.params.main_ingredient);
-        const { main_ingredient } = req.params;
-
-        const lists = await Recipes.findAll({
-            where: { main_ingredient },
-            include: [
-                {
-                    model: Users,
-                    attributes: ['user_name']
-                },
-                {
-                    model: Recipe_Img,
-                    where: { main_img: 1 },
-                    attributes: ['image_url'],
-                    required: false 
-                }
-            ],
-            attributes: ['title'],
-            order: [['createdAt', 'DESC']] 
-        });
-        res.json(lists);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-};
 
 
--- routes/Rindex.js
+++ routes/Rindex.js
@@ -1,8 +1,16 @@
-const controller=require('../controller/Cmain.js')
-const { setCookie } = require('../controller/Ccookie');
+const {main} =require('../controller/Cmain.js')
+const {setCookie} = require('../controller/Ccookie');
+const {getRecipeList} = require('../controller/Cmain.js');
 const express = require('express');
 const router=express.Router();
 
-router.get('/',controller.main);
+// main
+router.get('/', main);
+
+// 쿠키 설정
+router.get('/setCookie', setCookie);
+
+// (전체, 주재료) 레시피 리스트 
+// router.get('/:main_ingredient?', getRecipeList);
 
 module.exports = router;
