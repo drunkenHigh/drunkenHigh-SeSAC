81d0134ce405cfd9b738c102a1172e5ad1888bd3
AHNDAEUN <dean034@naver.com>
2024-07-15 17:16:29 +0900

    feat: user update

--- controller/Cusers.js
+++ controller/Cusers.js
@@ -1,5 +1,9 @@
 
-const { users } = require('../models/Muser')
+
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../models/Mindex');
+const UsersModel = require('../models/Muser');
+const Users = UsersModel(sequelize, DataTypes);
 
 
 
@@ -17,36 +21,41 @@ const { users } = require('../models/Muser')
 
        try {
 
-        const login = await users.fineOne({
+        const user = await Users.findOne({
 
             where:{
-                user_id: user_id,
-                user_pw: user_pw
-            }
+                user_id,
+                user_pw
+            },
+            attributes:['user_id','user_pw','user_name']
+
         })
 
-       
-        if(login.user_id !== user_id){
+        console.log('user >>>>>> ', user.user_id);
+
+         if(user.user_id !== user_id ){
+            // req.session.login = false;
+            // console.log('res >>>>>>>', res);
+            console.log('1 >>>>>>>' , user.user_id);
+            console.log('2 >>>>>>>' , user_id);
+            return res.status(404).json({ message: '등록되지 않은 사용자입니다.' });
+         } 
+
+        //  if(comparePw(user.user_pw !== user_pw)){
          
-            return res.status(404),json('등록 되지 않은 사용자 입니다.')
+        //      return res.status(404),json('비밀번호를 다시 확인해 주세요.')
 
-        } 
+        //  }
 
-        if(login.user_pw !== user_pw){
-         
-            return res.status(404),json('비밀번호를 다시 확인해 주세요.')
-
-        }
-
-        req.session.login = {
+        //  req.session.user = {
             
-            user_id : login.user_id,
-            user_pw: login.user_pw
+        //      user_id : req.body.user_id,
+        //      user_pw: req.body.user_pw
 
-        }
+        //  }
 
-        res.json('로그인 성공')
-        
+        //  console.log('req>>', req.session.user);
+
        } catch (error) {
         console.error(error);
          res.status(500).send('Internal Server Error');
@@ -79,54 +88,54 @@ const { users } = require('../models/Muser')
     //회원가입(POST)
     exports.postUsers = async (req, res) => {
           
-        try {
+        // try {
 
-            console.log(req.body)
-            const {user_id, user_name, user_pw,birth_day,profile_img} = req.body;
+        //     console.log(req.body)
+        //     const {user_id, user_name, user_pw,birth_day,profile_img} = req.body;
 
-           // 중복된 사용자 아이디 확인
-            const existUser = await users.findOne({
+        //    // 중복된 사용자 아이디 확인
+        //     const existUser = await users.findOne({
                
-                where: {
-                    user_id: user_id
-                }
-            });
+        //         where: {
+        //             user_id: user_id
+        //         }
+        //     });
 
-            if (existUser) {
-                return res.status(400).json('중복된 아이디 입니다.' );
-            }
+        //     if (existUser) {
+        //         return res.status(400).json('중복된 아이디 입니다.' );
+        //     }
 
-            if (!isValidPassword(user_pw)) {
-            return res.status(400).json('비밀번호는 대소문자, 특수문자, 숫자 포함하여 최소 8자 이상이어야 합니다.');
-            }
+        //     if (!isValidPassword(user_pw)) {
+        //     return res.status(400).json('비밀번호는 대소문자, 특수문자, 숫자 포함하여 최소 8자 이상이어야 합니다.');
+        //     }
 
-            if (!isValidBirthday(birth_day)) {
-            return res.status(400).json('올바른 생일 형식이 아닙니다. (예: YYYY-MM-DD)');
-             }
+        //     if (!isValidBirthday(birth_day)) {
+        //     return res.status(400).json('올바른 생일 형식이 아닙니다. (예: YYYY-MM-DD)');
+        //      }
 
 
 
-            //회원 생성
-            const newUser = await users.create({
-                user_id, user_name, profile_img, user_pw, birth_day
-            });
+        //     //회원 생성
+        //     const newUser = await users.create({
+        //         user_id, user_name, profile_img, user_pw, birth_day
+        //     });
 
-            res.json(newUser);
+        //     res.json(newUser);
 
-            res.send('myprofile',{
+        //     res.send('myprofile',{
 
-                user_name: req.body.user_name,
-                profile_img: profile_img,
-                birth_day:birth_day
-            })
+        //         user_name: req.body.user_name,
+        //         profile_img: profile_img,
+        //         birth_day:birth_day
+        //     })
 
           
 
 
-            } catch (error) {
-                console.error(error);
-                res.status(500).send('Internal Server Error');
-            }
+        //     } catch (error) {
+        //         console.error(error);
+        //         res.status(500).send('Internal Server Error');
+        //     }
             
 
     }
