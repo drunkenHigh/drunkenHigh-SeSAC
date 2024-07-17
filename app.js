7a44c013984cc21d507cb1f40c54fdba5405957c
AHNDAEUN <dean034@naver.com>
2024-07-17 10:33:26 +0900

    feate: login,logout

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require('express');
+const session = require('express-session');
 const app = express();
-const session = require('express-session')
 const router = require("./routes/Rindex");
 const router_rcp = require("./routes/Rrecipe");
 const router_users = require('./routes/Rusers');
 const { sequelize } = require("./models/Mindex");
 const path = require("path");
 const dotenv = require("dotenv");
+
 
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
@@ -19,26 +20,23 @@ app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", router);
-app.use('/recipe', router_rcp);
-app.use('/users', router_users);
 
 app.use(cookieParser());
 app.use("/public", express.static(__dirname + "/static"));
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-app.use(session({
-  secret : process.env.COOKIE_SECRET, // 필수 옵션, 세션 암호화하는데 사용하는 키
-  resave : false, // 세션이 변경되지 않더라도 매번 덮어쓰기로 저장을 할건지 여부 설정 옵션
-  saveUninitialized : false, // 세션을 초기값이 지정되지 않은 상태에서도 처음부터 세션을 생성할 건지 여부 설정 옵션
-  // 세션 쿠키 설정(세션관리할 때 클라이언트로 보내는 쿠키)
-  cookie : {
-      httpOnly : true, // 클라이언트에서 쿠키 확인 X
-      secure : false, // http에서 사용가능하도록 true면 https에서만 가능
-      maxAge : 60 * 1000 // 단위(ms), expires : 만료기간 설정
-  }
-}))
 
+app.use(session({
+  secret :process.env.COOKIE_SECRET, 
+  resave : false, 
+  saveUninitialized : false, 
+  cookie : {
+    httpOnly :true,
+    secure : false,
+    expires : 1800 * 1000
+}
+  
+}))
 
 dotenv.config({
   path: path.resolve(__dirname, ".env"),
@@ -53,6 +51,12 @@ dotenv.config({
 const port = process.env.PORT;
 const dbName = process.env.DATABASE_NAME;
 const dbPw = process.env.DATABASE_PW;
+
+
+
+app.use("/", router);
+app.use('/recipe', router_rcp);
+app.use('/users', router_users);
 
 // app.listen(port, () => {
 //   console.log(`Server running...PORT: ${port}`);
